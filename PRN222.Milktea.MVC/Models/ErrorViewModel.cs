f1fb13377cc27627401567ca8b60b53d3b010ce8
BeeHunter-247 <Ngoclong24072003@gmail.com>
2025-03-20 09:09:06 +0700

    switch to Blazor

--- PRN222.Milktea.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PRN222.Milktea.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- PRN222.Milktea.Service/Services/Interfaces/IAccountService.cs
+++ PRN222.Milktea.Service/Services/Interfaces/IAccountService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PRN222.Milktea.Service.BusinessModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace PRN222.Milktea.Service.Services.Interfaces
 {
     public interface IAccountService
     {
+        Task<AccountModel> Login(string email, string password);
     }
 }
