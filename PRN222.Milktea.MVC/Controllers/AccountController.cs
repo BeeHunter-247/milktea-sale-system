27cca1a494be0421b58c37828c98b2c522925b10
Duychamchi <duynhon2106@gmail.com>
2025-04-03 15:04:53 +0700

    Fix
    Account Controlle

--- PRN222.Milktea.MVC/Controllers/AccountController.cs
+++ PRN222.Milktea.MVC/Controllers/AccountController.cs
@@ -1,54 +1,55 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PRN222.Milktea.MVC.Models;
-using PRN222.Milktea.Service.Services;
 using PRN222.Milktea.Service.Services.Interfaces;
 using System.Security.Claims;
 
 namespace PRN222.Milktea.MVC.Controllers
 {
-	public class AccountController : Controller
-	{
-		private readonly IAccountService _accountService;
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _accountService;
 
-		public AccountController(IAccountService accountService)
+        public AccountController(IAccountService accountService)
         {
-			_accountService = accountService;
-		}
+            _accountService = accountService;
+        }
 
         public string ErrorMessage { get; set; }
 
         [HttpGet]
-		public IActionResult Login()
-		{
-			if(User.Identity != null && User.Identity.IsAuthenticated)
-			{
+        public IActionResult Login()
+        {
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
                 return RedirectToAction("Index", "Products");
-
             }
 
             return View();
         }
 
-		[HttpPost]
-		public async Task<IActionResult> Login(LoginViewModel loginViewModel)
-		{
-			if (!ModelState.IsValid){
-				return RedirectToAction("Login");
-			}
-			var account = await _accountService.Login(loginViewModel.Email, loginViewModel.Password);
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Login");
+            }
 
-			if(account == null)
-			{
-				ErrorMessage = "Invalid email or password. Please try again!";
-				return NotFound();
-			}
+            var account = await _accountService.Login(loginViewModel.Email, loginViewModel.Password);
 
-			if(account.AccountRole != 1)
-			{
-				return RedirectToAction("AccessDenied");
-			}
+            if (account == null)
+            {
+                ErrorMessage = "Invalid email or password. Please try again!";
+                return NotFound();
+            }
+
+            if (account.AccountRole != 1)
+            {
+                return RedirectToAction("AccessDenied");
+            }
 
             var claims = new List<Claim>
             {
@@ -57,7 +58,7 @@ namespace PRN222.Milktea.MVC.Controllers
                 new Claim(ClaimTypes.Role, account.AccountRole.ToString())
             };
 
-            var claimsIdentify = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
             var authProperties = new AuthenticationProperties
             {
@@ -65,30 +66,47 @@ namespace PRN222.Milktea.MVC.Controllers
                 ExpiresUtc = DateTime.UtcNow.AddMinutes(10),
             };
 
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentify), authProperties);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
 
-            return RedirectToAction("Index","Products");
-		}
+            return RedirectToAction("Index", "Products");
+        }
 
-		[HttpGet]
-		public IActionResult AccessDenied()
-		{
-			return View();
-		}
+        [HttpGet]
+        [Authorize(Roles = "1")]
+        public async Task<IActionResult> Index()
+        {
+            var accountIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (accountIdClaim == null)
+            {
+                return Unauthorized();
+            }
+
+            int currentAdminId = int.Parse(accountIdClaim.Value);
+
+            var accounts = await _accountService.GetAllAccountsExceptAdminAsync(currentAdminId);
+            return View(accounts);
+        }
 
         [HttpPost]
+        [Authorize(Roles = "1")]
         public async Task<IActionResult> Ban(int accountId)
         {
             await _accountService.BanAccountAsync(accountId);
             return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
 
-
+        [HttpPost]
         public async Task<IActionResult> Logout()
-		{
+        {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Login");
-		}
-	}
+        }
+    }
 }
